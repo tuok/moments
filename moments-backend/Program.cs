00c835b7f77035b465e755a5d89b0e357a30b8f1
Tuomas Kujala <tuomas@kujala.fi>
2018-08-01 10:34:57 +0300

    Add .NET Core project stub.

---
+++ moments-backend/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace moments_backend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
