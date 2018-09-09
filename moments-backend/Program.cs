ed05b858dd73b9b49159449f3799751230b5d4bf
Tuomas Kujala <tuomas@kujala.fi>
2018-09-09 13:19:45 +0300

    Print error message on backend start instead of throwing uncaught exception.

--- moments-backend/Program.cs
+++ moments-backend/Program.cs
@@ -15,7 +15,10 @@ namespace Moments
         public static void Main(string[] args)
         {
             if (args.Length < 1)
-                throw new ArgumentException("Path to entries root folder has to be given as an argument.");
+            {
+                Console.WriteLine("Path to entries root folder has to be given as an argument.");
+                return;
+            }
 
             Console.WriteLine($"Entry root path '{args[0]}' was provided.");
             args[0] = "entryPath=" + args[0];
