70aa533db8a38a248c4e0b55227cef1a9de714a7
Tuomas Kujala <tuomas@kujala.fi>
2018-09-19 21:59:05 +0300

    - Disable closing of the dialog by clicking background or pressing escape. - Add some dummy content to EntryDialog.

--- moments-frontend/src/EntryDialog.js
+++ moments-frontend/src/EntryDialog.js
@@ -5,6 +5,7 @@ import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
+import { TextField, Button } from '@material-ui/core';
 
 export default class EntryDialog extends React.Component {
   constructor(props) {
@@ -20,8 +21,30 @@ export default class EntryDialog extends React.Component {
       <Dialog
         open={this.props.opened}
         onClose={e => this.props.handleClose(false, null)}
+        disableBackdropClick={true}
+        disableEscapeKeyDown={true}
       >
         <DialogTitle>Kirjauksen muokkaus</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Tässä näkymässä voit luoda uusia kirjauksia tai muokata olemassa olevia kirjauksia.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="text"
+            label="Kirjauksen teksti"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={e => this.props.handleClose(false, null)}>
+            Peruuta
+          </Button>
+          <Button onClick={e => this.props.handleClose(false, null)}>
+            Lisää kirjaus
+          </Button>
+        </DialogActions>
       </Dialog>
     )
   }
