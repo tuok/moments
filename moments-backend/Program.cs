bc757418118d2d9b5a8499a65dda282a29690f72
Tuomas Kujala <tuomas@kujala.fi>
2018-12-01 11:43:44 +0200

    Serve from 0.0.0.0:5000 instead of localhost:5000.

--- moments-backend/Program.cs
+++ moments-backend/Program.cs
@@ -29,6 +29,7 @@ namespace Moments
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:5000/")
                 .Build();
     }
 }
