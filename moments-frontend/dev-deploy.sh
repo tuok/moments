f97220a1dddec76c8780cdee50ac2c9ca84216d2
Tuomas Kujala <tuomas@scandit.com>
2025-01-04 18:45:29 +0200

    Update frontend dev deploy script

--- moments-frontend/dev-deploy.sh
+++ moments-frontend/dev-deploy.sh
@@ -1,4 +1,5 @@
+npm install
 npm run build
-rm -r ../moments-backend-python/static || true
+rm -rf ../moments-backend-python/static || true
 mkdir -p ../moments-backend-python/static
 cp -r dist/* ../moments-backend-python/static/
